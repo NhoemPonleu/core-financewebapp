2d354ada54230bfb21c5cf4eb5b9014e23cadb4e
leuleu <surveybox75@gmail.com>
2024-02-27 20:49:46 +0700

    first commit

---
+++ src/main/java/com/example/corefinance/CoreFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.corefinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/corefinance/CoreFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
